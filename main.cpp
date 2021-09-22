aedfbfcc128e391ab52033cbfb4bf34d024b94f8
Jon <jberez@live.ca>
2021-09-22 20:01:09 +0200

    Lightweight coin implemented

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Cryptocurrency
+
+#include <iostream>
+
+void do_cryptocurrency() {
+    std::cout << "Cryptocurrency processing... \n";
+}
+
+int main() {
+    do_cryptocurrency();
+    std::cout << "Cryptocurrency complete.";
+    return 0;
+}

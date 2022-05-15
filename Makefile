e9e62a1f70b6f08db6e99f5869a41a89c00202f6
cdifulton <cdifulton@gmail.com>
2022-05-15 09:48:15 -0400

    Reorganized: src and test directories, makefile updated

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 outfile = scrumcoin
+test_file = test_exec
 
 all: main.cpp
-	g++ -o ${outfile} -Iinclude *.cpp
+	g++ -o ${outfile} -Iinclude *.cpp src/*.cpp
 
 run: all
 	./${outfile}
 
 clean: 
 	$(RM) ${outfile}
+
+test:
+	g++ -o tests/${test_file} -Iinclude tests/test1.cpp src/*.cpp
+	tests/${test_file}
---
+++ tests/test1.cpp
@@ -0,0 +1,36 @@
+// ScrumCoin
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <string>
+
+#include <Blockchain.h>
+
+using std::string;
+using std::to_string;
+
+void do_cryptocurrency() {
+    std::cout << "Cryptocurrency processing... \n";
+};
+
+int main() {
+    // It Begins
+    Blockchain ScrumCoin;
+
+    // Data for Prime Block
+    Transaction data1;
+    time_t data1Time;
+    data1.amount = 100.0;
+    data1.receiverKey = "Joey Prime";
+    data1.senderKey = "Mayer Joey";
+    data1.timestamp = time(&data1Time);
+
+    ScrumCoin.add_block(data1);
+
+    std::cout << "Is chain valid?" << std::endl
+        << ScrumCoin.is_chain_valid() << std::endl;
+
+    std::cout << "Cryptocurrency complete. \n";
+    return 0;
+}
+
